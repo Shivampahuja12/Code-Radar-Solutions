98ecef1da2287379b5a3b35b12dd3b48626a17b9
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-01-21 13:04:51 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -4,13 +4,13 @@ int main(){
     if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=="I" || c=="O" || c=="U"){
         printf("Vowel");
     }
-    else if (((c>='a' && c<='z') && (c>='A' && c<='Z')) && (c!='a' || c!='e' || c!='i'|| c!='o' || c!='u' || c!='A' || c!='E' || c!='I' || c!='O' || c!='U')){
+    if (((c>='a' && c<='z') && (c>='A' && c<='Z')) && (c!='a' || c!='e' || c!='i'|| c!='o' || c!='u' || c!='A' || c!='E' || c!='I' || c!='O' || c!='U')){
         printf("Consonant");
     }
-    else if (c>='1' && c<='9'){
+    if (c>='1' && c<='9'){
         printf("Digit");
     }
-    else {
+     {
         printf("Special Character");
     }
     return 0;
