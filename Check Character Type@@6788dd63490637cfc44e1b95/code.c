0d03fa70129884a3c98d60a01c075ba8051af882
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-01-23 20:56:12 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -2,17 +2,21 @@
 int main(){
     char c;
     scanf("%c", &c);
-    if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=="I" || c=="O" || c=="U"){
-        printf("Vowel");
-    }
-    else if (((c>='a' && c<='z') || (c>='A' && c<='Z')) && (c!='a' || c!='e' || c!='i'|| c!='o' || c!='u' || c!='A' || c!='E' || c!='I' || c!='O' || c!='U')){
-        printf("Consonant");
-    }
-    else if (c>='1' && c<='9'){
-        printf("Digit");
+    if ((c>='a' && c<='z') || (c>='A' && c<='Z')){
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
+            printf("Vowel");
+        }
+        else {
+            printf("Consonant");
+        }
     }
     else {
-        printf("Special Character");
+        if (c>='1' && c<='9'){
+            printf("Digit");
+        }
+        else {
+            printf("Special Character");
+        }
     }
     return 0;
 }
