11499bb1e71305f54cf7fda24e70559a22c627eb
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-03-07 14:44:37 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -3,7 +3,7 @@
 #include<stdbool.h>
 int isPrime(int n){
     int flag = true;
-    if (n==1) return 0;
+    if (n==0 || n==1) return 0;
     for (int i=2; i<n; i++){
         if (n%i==0) {
             flag = false;
