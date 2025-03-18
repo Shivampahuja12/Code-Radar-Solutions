73ac882f32a32f37d55ea9d8b91362ce9143ad64
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-03-18 22:24:36 +0530

    Solution to Selection Sort for Characters

---
+++ Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void swapper(char *a, char *b){
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main(){
+    char a[] = {'b', 'r', 'a', 'l', 'e', 'o'};
+    for (int i=1; i<6; i++){
+        char j = i;
+        while (j>=1 && a[j] < a[j-1]){
+            if (a[j] >= a[j-1]){
+                break;
+            }
+            else {
+                swapper(&a[j], &a[j-1]);
+            }
+            j--;
+        }
+    }
+}
+void printArray(int a[], int n){
+    for (int i=0; i<n; i++){
+        printf("%d ", a[i]);
+    }
+}
